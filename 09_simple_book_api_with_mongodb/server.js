56cc60fc05abd85e329e4ea2141e9919bd219b5b
justinemarco <justinejynnepatricemarco@student.laverdad.edu.ph>
2025-05-27 18:16:19 +0800

    Submission for Assignment #9

---
+++ 09_simple_book_api_with_mongodb/server.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Book = require('./models/Book');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+// const PORT = 3000;
+const mongo_URI = process.env.mongo_URI;
+const PORT = process.env.PORT || 3000;
+require('dotenv').config();
+
+// MIDDLEWARE
+app.use(express.json()); 
+
+mongoose.connect(process.env.mongo_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+const dataFile = path.join(__dirname, 'books.json');
+
+function loadBooks() {
+    const data = fs.readFileSync(dataFile, 'utf-8');
+    return JSON.parse(data);
+}
+
+function saveBooks(books) {
+    fs.writeFileSync(dataFile, JSON.stringify(books, null, 2));
+};
+
+// GET
+app.get('/', (req, res) => {
+    res.send('Simple Book API using Node.js and Express');
+});
+
+// GET /api/books
+app.get('/api/books', async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ message: 'Error fetching books' });
+  }
+});
+
+// GET /api/books/:id
+app.get('/api/books/:id', async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) return res.status(404).json({ message: 'Book not found' });
+    res.json(book);
+  } catch (err) {
+    res.status(400).json({ message: 'Invalid book ID' });
+  }
+});
+
+// POST /api/books
+app.post('/api/books', async (req, res) => {
+  const { title, author } = req.body;
+  if (!title || !author) {
+    return res.status(400).json({ message: 'Title and author are required' });
+  }
+
+  try {
+    const newBook = new Book({ title, author });
+    const savedBook = await newBook.save();
+    res.status(200).json(newBook);
+  } catch (err) {
+    res.status(500).json({ message: 'Error creating book' });
+  }
+});
+
+// PATCH /api/books/:id
+app.patch('/api/books/:id', async (req, res) => {
+  try {
+    const updates = req.body;
+    const book = await Book.findByIdAndUpdate(req.params.id, updates, { new: true });
+    if (!book) return res.status(404).json({ message: 'Book not found' });
+    res.json(book);
+  } catch (err) {
+    res.status(400).json({ message: 'Invalid update or ID' });
+  }
+});
+
+// DELETE /api/books/:id
+app.delete('/api/books/:id', async (req, res) => {
+  try {
+    const result = await Book.findByIdAndDelete(req.params.id);
+    if (!result) return res.status(404).json({ message: 'Book not found' });
+    res.json({ message: 'Book deleted successfully' });
+  } catch (err) {
+    res.status(400).json({ message: 'Invalid ID' });
+  }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+})
